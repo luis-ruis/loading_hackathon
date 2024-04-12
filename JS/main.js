23d14bbbe8ff6cd30b45d04f0d48a986fdba2215
germancitom <gcruzm14158@gmail.com>
2024-04-12 16:26:27 -0600

    sincronisacionde semaforo

--- JS/main.js
+++ JS/main.js
@@ -2,25 +2,24 @@ var colorRojo = false;
 var colorVerde = false;
 
 class Car {
-  constructor(carretera,posicionInicial,posicionFinal,contrario) {
+  constructor(carretera, posicionInicial, posicionFinal, contrario) {
     this.carretera = carretera;
     this.car = this.createCar();
-    this.posicionInicial=posicionInicial;
-    this.posicionFinal=posicionFinal;
-    this.contrario=contrario;
+    this.posicionInicial = posicionInicial;
+    this.posicionFinal = posicionFinal;
+    this.contrario = contrario;
     this.dx = 0; // Velocidad horizontal constante
     this.dy = 2; // Velocidad vertical (0 para que no se mueva verticalmente)
     this.crossedTrafficLight = false; // Indica si el carro ya cruzó el semáforo
     this.stopped = false; // Indica si el carro está detenido
-    
   }
   //se crea el carro
   createCar() {
     const car = document.createElement("div");
     car.classList.add("car");
     car.style.left = "10px";
-    console.log(this.posicionFinal)
-    car.style.top = "750px";
+    console.log(this.posicionFinal);
+    car.style.top = "0px";
     this.carretera.appendChild(car);
     return car;
   }
@@ -32,18 +31,17 @@ class Car {
   move() {
     if (!this.stopped) {
       let left = parseFloat(this.car.style.left) + this.dx;
-      
 
       if (top > this.carretera.offsetHeight) {
         this.car.remove();
         return false;
       } else {
-        if(this.contrario){
+        if (this.contrario) {
           let top = parseFloat(this.car.style.top) + this.dy;
           this.car.style.left = left + "px";
           this.car.style.top = top + "px";
         }
-        if(!this.contrario){
+        if (!this.contrario) {
           let top = parseFloat(this.car.style.top) - this.dy;
           this.car.style.left = left + "px";
           this.car.style.top = top + "px";
@@ -84,20 +82,25 @@ class TrafficLight {
 }
 
 class RoadSimulator {
-  constructor(carretera, circle,posicionInicial,posicionFinal,contrario) {
+  constructor(carretera, circle, posicionInicial, posicionFinal, contrario) {
     this.carretera = carretera;
     this.circle = circle;
-    this.contador=0;
-    this.posicionInicial=posicionInicial;
-    this.posicionFinal=posicionFinal;
-    this.contrario=contrario;
+    this.contador = 0;
+    this.posicionInicial = posicionInicial;
+    this.posicionFinal = posicionFinal;
+    this.contrario = contrario;
     this.carsArray = [];
     this.addRandomCar();
     setInterval(() => this.moveCars(), 50);
   }
   //funcion para agregar carros en la calle
   addRandomCar() {
-    const car = new Car(this.carretera,this.posicionInicial,this.posicionFinal,this.contrario);
+    const car = new Car(
+      this.carretera,
+      this.posicionInicial,
+      this.posicionFinal,
+      this.contrario
+    );
     this.carsArray.push(car);
     // Añadir un nuevo carro en un intervalo aleatorio entre 800ms y 4000ms
     setTimeout(() => this.addRandomCar(), this.randomInterval(800, 4000));
@@ -107,39 +110,45 @@ class RoadSimulator {
     const trafficLightPosition = this.circle.getBoundingClientRect();
     const trafficLightHeight =
       trafficLightPosition.top + trafficLightPosition.height;
+    if (this.contrario) {
+      this.carsArray.forEach((car, index) => {
+        const carPosition = car.car.getBoundingClientRect();
+        const carHeight = carPosition.top + carPosition.height;
 
-    this.carsArray.forEach((car, index) => {
-      const carPosition = car.car.getBoundingClientRect();
-      const carHeight = carPosition.top + carPosition.height;
+        const shouldStop =
+          (colorRojo &&
+            carHeight >= trafficLightHeight &&
+            !car.crossedTrafficLight) ||
+          (index > 0 && this.shouldStop(car, index));
 
-      const shouldStop =
-        (colorRojo &&
-          carHeight >= trafficLightHeight &&
-          !car.crossedTrafficLight) ||
-        (index > 0 && this.shouldStop(car, index));
-
-      if (shouldStop) {
-        car.stop();
-      } else {
-        if (car.stopped && index > 0 && !this.carsArray[index - 1].stopped) {
-          car.stopped = false;
+        if (shouldStop) {
+          car.stop();
+        } else {
+          if (car.stopped && index > 0 && !this.carsArray[index - 1].stopped) {
+            car.stopped = false;
+          }
+          car.move();
+          if (carHeight >= trafficLightHeight) {
+            car.crossedTrafficLight = true;
+          }
         }
-        car.move();
-        if (carHeight >= trafficLightHeight) {
-          car.crossedTrafficLight = true;
+        //cuenta el numero de carros que pasa cunado el semaforo esta en verde
+        if (
+          colorVerde &&
+          car.crossedTrafficLight &&
+          carHeight > 460 &&
+          carHeight < 462
+        ) {
+          this.contador++;
+          console.log("contaodr: ", carHeight);
+          console.log("co: ", this.contador);
         }
-      }
-      //cuenta el numero de carros que pasa cunado el semaforo esta en verde
-      if(colorVerde&&car.crossedTrafficLight&&carHeight>460&& carHeight<462){
-        this.contador++;
-        console.log("contaodr: ",carHeight);
-        console.log("co: ",this.contador);
-      }
-      // el contador se reinicia 
-      if(colorRojo&&carHeight>465){
-        this.contador=0;
-      }
-    });
+        // el contador se reinicia
+        if (colorRojo && carHeight > 465) {
+          this.contador = 0;
+        }
+      });
+    }
   }
   //funcion para que el carro se detenga
   shouldStop(car, currentIndex) {
@@ -169,35 +178,29 @@ class RoadSimulator {
 }
 
 const carretera1 = document.getElementById("carretera1");
-carretera1.style.height="800px";
-carretera1.style.left="400px";
-carretera1.style.width="50px";
+carretera1.style.height = "800px";
+carretera1.style.left = "400px";
+carretera1.style.width = "50px";
 const circle = document.getElementById("circle");
 
-const roadSimulator = new RoadSimulator(carretera1, circle,10,0,false);
+const roadSimulator = new RoadSimulator(carretera1, circle, 10, 0, true);
 const trafficLight = new TrafficLight(circle);
 
-
-
-
-
 const carretera2 = document.getElementById("carretera2");
-        carretera2.style.height = "800px";
-        carretera2.style.left = "340px";
-        carretera2.style.top = "-820px";
-        carretera2.style.width = "50px";
+carretera2.style.height = "800px";
+carretera2.style.left = "340px";
+carretera2.style.top = "-820px";
+carretera2.style.width = "50px";
 
-        const circle2 = document.createElement("div");
-        circle2.style.width = "20px";
-        circle2.style.height = "20px";
-        circle2.style.left = "-25px";
-        circle2.style.borderRadius = "50%";
-        circle2.style.backgroundColor = "green";
-        circle2.style.position = "relative";
+const circle2 = document.createElement("div");
+circle2.style.width = "20px";
+circle2.style.height = "20px";
+circle2.style.left = "-25px";
+circle2.style.borderRadius = "50%";
+circle2.style.backgroundColor = "red";
+circle2.style.position = "relative";
 
-        carretera2.appendChild(circle2);
+carretera2.appendChild(circle2);
 
-        const roadSimulator2 = new RoadSimulator(carretera2, circle2,10,0,false);
-        const trafficLight2 = new TrafficLight(circle2);
-
-
+const roadSimulator2 = new RoadSimulator(carretera2, circle2, 10, 0, false);
+const trafficLight2 = new TrafficLight(circle2);
