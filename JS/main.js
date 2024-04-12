90467dae4e5c75e040e38c00ba8e4f9672390d91
germancitom <gcruzm14158@gmail.com>
2024-04-12 08:49:03 -0600

    aplicando POO

--- JS/main.js
+++ JS/main.js
@@ -1,74 +1,93 @@
+class Car {
+  constructor(carretera) {
+      this.carretera = carretera;
+      this.car = this.createCar();
+      this.dx = 0; // Velocidad horizontal constante
+      this.dy = 2; // Velocidad vertical (0 para que no se mueva verticalmente)
+  }
+
+  createCar() {
+      const car = document.createElement("div");
+      car.classList.add("car");
+      car.style.left = "10px";
+      car.style.top = "0px";
+      this.carretera.appendChild(car);
+      return car;
+  }
+
+  move() {
+      let left = parseFloat(this.car.style.left) + this.dx;
+      let top = parseFloat(this.car.style.top) + this.dy;
+
+      if (top > this.carretera.offsetHeight) {
+          this.car.remove();
+          return false;
+      } else {
+          this.car.style.left = left + "px";
+          this.car.style.top = top + "px";
+          console.log("El valor de la variable es:", this.car.style.top);
+          return true;
+      }
+  }
+}
+//clase que crea el semaforo y controla los colores 
+class TrafficLight {
+  constructor(circle) {
+      this.circle = circle;
+      this.miVariable = false;
+      setInterval(() => this.alternateVariable(), 3000);
+      setInterval(() => this.changeColor(), 3000);
+  }
+
+  alternateVariable() {
+      this.miVariable = !this.miVariable;
+      console.log("El valor de la variable es:", this.miVariable);
+  }
+
+  changeColor() {
+      if (this.circle.style.backgroundColor === 'green') {
+          this.circle.style.backgroundColor = 'red';
+      } else {
+          this.circle.style.backgroundColor = 'green';
+      }
+  }
+}
+
+class RoadSimulator {
+  constructor(carretera, circle) {
+      this.carretera = carretera;
+      this.circle = circle;
+      this.carsArray = [];
+      this.addRandomCar();
+      setInterval(() => this.moveCars(), 50);
+  }
+
+  addRandomCar() {
+      const car = new Car(this.carretera);
+      this.carsArray.push(car);
+      setTimeout(() => this.addRandomCar(), this.randomInterval(700, 2000));
+  }
+
+  moveCars() {
+      this.carsArray.forEach((car, index) => {
+          /*if (!this.miVariable && car.car.style.top < 400 + "px") {
+              if (!car.move()) {
+                  this.carsArray.splice(index, 1);
+              }else{
+                car.move();
+              }
+          }*/
+          car.move();
+      });
+  }
+
+  randomInterval(min, max) {
+      return Math.random() * (max - min) + min;
+  }
+}
+
 const carretera = document.getElementById("carretera");
-let carCount = 0;
-let carsArray = []; // Array para almacenar las pelotas
-function createCar() {
-  const car = document.createElement("div");
-  car.classList.add("car");
-  car.style.left = "10px";
-  car.style.top = "0px";
-  carretera.appendChild(car);
-  carCount++;
-  carsArray.push(car); // Agregar el carro al array
-}
+const circle = document.getElementById('circle');
 
-let prevTop = 0; // Variable para almacenar la posición top del carro anterior
-let numero=400;
-let posicion;
-function moveCars() {
-  const balls = document.querySelectorAll(".car");
-  carsArray.forEach((car, index) => {
-    const dx = 0; // Velocidad horizontal constante
-    const dy = 2; // Velocidad vertical (0 para que no se mueva verticalmente)
-    let left = parseFloat(car.style.left) + dx;
-    let top = parseFloat(car.style.top) + dy;
-
-    // Eliminar el carro en la carretera
-    if (top > carretera.offsetHeight) {
-      car.remove();
-      carCount--;
-      carsArray.splice(index, 1); // Eliminar el carro del array
-    } else {
-      //carros que avanzan antes del rojo
-      if(!miVariable&&car.style.top < numero+"px"){
-        car.style.left = left + "px";
-        car.style.top = top + "px";
-      }
-      // carros que se detienen en rojo
-      if (miVariable) {
-        car.style.left = left + "px";
-        car.style.top = top + "px";
-      }
-      // carros que abanzan despues del semaforo
-      if (car.style.top > 400 + "px") {// defina la pocion de semaforo
-        car.style.top = top + "px";
-      }
-    }
-  });
-}
-
-function randomInterval(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-function addRandomCar() {
-  createCar();
-  setTimeout(addRandomCar, randomInterval(700, 2000)); // Añadir un nuevo carro en un intervalo aleatorio entre 500ms y 2000ms
-}
-
-// Comenzar agrregando el primer carro
-addRandomCar();
-
-// Mover el carro cada 50 milisegundos
-setInterval(moveCars, 50);
-
-//---------------------------------------------------------------------------
-// Función para cambiar la variable a verdadero después de 5 segundos
-let miVariable = false;
-// Función para alternar el valor de la variable entre falso y verdadero
-function alternarVariable() {
-  miVariable = !miVariable; // Alternar el valor entre falso y verdadero
-  console.log("El valor de la variable es:", miVariable);
-}
-
-// Llamar a la función cada 2 segundos (2000 milisegundos)
-setInterval(alternarVariable, 3000); // Cambiar cada 2 segundos
+const roadSimulator = new RoadSimulator(carretera, circle);
+const trafficLight = new TrafficLight(circle);
